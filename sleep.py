6503f53b3c54f3509a0fd36491ba2e8bb551d0d3
John Poole <john@primatelabs.com>
2017-07-23 16:46:43 -0400

    Add agent sleep script

---
+++ sleep.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# Agent Scripts
+#
+# Copyright (C) 2004-2017 Primate Labs Inc.  All rights reserved.
+
+import os, subprocess, sys
+
+def sleep_linux():
+  # TODO
+  pass
+
+def sleep_macosx():
+  subprocess.call(['pmset', 'sleepnow'])
+
+def sleep_windows():
+  subprocess.call([
+    'reg.exe', 'ADD', '"HKCU\Software\Sysinternals\PsShutdown"',
+    '/v', 'EulaAccepted',
+    '/t', 'REG_DWORD',
+    '/d', '1',
+    '/f'])
+  subprocess.call([
+    'windows\\psshutdown.exe', '-d', '-t', '0'
+  ])
+
+
+def main():
+  platforms = {
+    'darwin' : 'macosx',
+    'linux2' : 'linux',
+    'win32' : 'windows',
+  }
+
+  force = True
+
+  if force or (os.environ.has_key('AGENT_SLEEP') and int(os.environ['AGENT_SLEEP'])):
+    print "shutdown"
+    platform = platforms[sys.platform]
+    globals()[ 'shutdown_%s' % (platform,) ]()
+
+if __name__ == '__main__':
+  main()
